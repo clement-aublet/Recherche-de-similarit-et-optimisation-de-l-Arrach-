f80b259b705024d86b9b35d54d7d040ae11ab7a0
aublet <clement.aublet@groupe-esigelec.org>
2024-03-22 21:14:26 -0400

    fichier python ImportationVideo + création fichier json metadonnées incomplet

---
+++ ImportationVideo.py
@@ -0,0 +1,54 @@
+from pytube import Playlist
+import cv2
+import os
+
+def telechargement_video(url, path='videos'):   # création du dossier vidéos où nous stockerons ces dernières
+    from pytube import YouTube
+    yt = YouTube(url)
+    # Choix du stream avec la résolution souhaitée ou le premier stream vidéo disponible
+    ys = yt.streams.filter(progressive=True, file_extension='mp4').first()
+    # Téléchargez la vidéo dans le dossier spécifié
+    if not os.path.exists(path):
+        os.makedirs(path)
+    out_file = ys.download(output_path=path)
+    print(f"La vidéo a été téléchargée et enregistrée sous {out_file}")
+    return out_file
+
+def lecture_video(video_path):
+    # Utilisation de OpenCV pour lire la vidéo
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Erreur lors de l'ouverture de la vidéo")
+        return
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Erreur lors de la lecture de la vidéo ou fin de la vidéo")
+            break
+
+        # Affichez le frame actuel
+        cv2.imshow('Frame', frame)
+
+        # Quitter la boucle/changer de vidéos en appuiyant sur 'q'
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def telechargement_et_lecture_playlist(playlist_url, path='videos'):
+    playlist = Playlist(playlist_url)
+    print(f"Téléchargement et lecture des vidéos de la playlist: {playlist.title}")
+    for video_url in playlist.video_urls:
+        print(f"Téléchargement de : {video_url}")
+        try:
+            video_path = telechargement_video(video_url, path)
+            print(f"Lecture de : {video_path}")
+            lecture_video(video_path)
+        except Exception as e:
+            print(f"Erreur lors du téléchargement ou de la lecture de la vidéo {video_url}: {e}") # exception pour voir si cela ne fonctionne pas
+
+#L'URL de la playlist YouTube test
+playlist_url = 'https://www.youtube.com/playlist?list=PL3hyy4ipA8G3sbSfny9ka5z_lfink2DQd'
+telechargement_et_lecture_playlist(playlist_url)
---
+++ metadonnees.py
@@ -0,0 +1,57 @@
+# Création du fichier json pour stocker les données
+# Liste pour stocker les chemins complets des fichiers vidéo
+
+#import ImportationVideo
+import os
+import re
+import json
+
+dossier_videos = 'C:/Users/Clément Aublet/OneDrive - ESIGELEC/Bureau/projetSeminaire/Recherche-de-similarit-et-optimisation-de-l-Arrach-/videos/'
+fichiers_videos = [os.path.join(dossier_videos, fichier) for fichier in os.listdir(dossier_videos) if
+                   fichier.endswith('.mp4')]
+
+
+def extraire_metadonnees_enrichies(nom_fichier):
+    pattern = re.compile(r'(?P<nom>.+) \((?P<poids_cat>\d+(?:\.\d+)?kg) (?P<pays>[^\)]+)\) (?P<description>.+)')
+    match = pattern.search(nom_fichier)
+
+    if match:
+        metadata = match.groupdict()
+
+        # Extraction supplémentaire pour le poids soulevé et le type de mouvement
+        detail_match = re.search(r'(\d+)kg (Snatch|Clean & Jerk|Squat)', nom_fichier, re.IGNORECASE)
+        if detail_match:
+            metadata["poids_souleve"] = detail_match.group(1) + "kg"
+            metadata["type_mouvement"] = detail_match.group(2)
+        else:
+            metadata["poids_souleve"] = "Inconnu"
+            metadata["type_mouvement"] = "Inconnu"
+
+
+        if "woman" in nom_fichier.lower() or "female" in nom_fichier.lower() or any(
+                word in nom_fichier for word in ["Madame", "Miss", "Mrs"]):
+            metadata["sexe"] = "Femme"
+        elif "man" in nom_fichier.lower() or "male" in nom_fichier.lower() or any(
+                word in nom_fichier for word in ["Monsieur", "Mr"]):
+            metadata["sexe"] = "Homme"
+        else:
+            metadata["sexe"] = "Inconnu"
+
+        return metadata
+    else:
+        return {}
+
+
+metadonnees_enrichies = []
+
+for fichier in fichiers_videos:
+    nom_fichier = os.path.basename(fichier)
+    metadata_enrichie = extraire_metadonnees_enrichies(nom_fichier)
+    metadonnees_enrichies.append(metadata_enrichie)
+
+chemin_json_enrichi = 'metadonnees_videos_enrichies.json'
+
+with open(chemin_json_enrichi, 'w', encoding='utf-8') as fichier_json:
+    json.dump(metadonnees_enrichies, fichier_json, ensure_ascii=False, indent=4)
+
+print(f"Les métadonnées enrichies ont été sauvegardées dans '{chemin_json_enrichi}'.")
